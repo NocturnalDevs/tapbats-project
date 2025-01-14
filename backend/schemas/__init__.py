576a47264893d8bb7647700091e8d8169286b6e6
MarkVincentLanada <markvincentdesacola@gmail.com>
2025-01-14 15:04:33 +0800

    fix 3

---
+++ backend/schemas/__init__.py
@@ -0,0 +1,15 @@
+# schemas/__init__.py
+from .schemas import (
+    UserBase, UserCreate, User,
+    UserFundsBase, UserFundsCreate, UserFunds,
+    UserTapMiningBase, UserTapMiningCreate, UserTapMining,
+    UserSocialsBase, UserSocialsCreate, UserSocials,
+    QuestBase, QuestCreate, Quest,
+    UserQuestBase, UserQuestCreate, UserQuest,
+    UserElderBase, UserElderCreate, UserElder,
+    UserMembersBase, UserMembersCreate, UserMembers,
+    CavernBase, CavernCreate, Cavern,
+    UserCavernBase, UserCavernCreate, UserCavern,
+    MinerBase, MinerCreate, Miner,
+    UserMinerBase, UserMinerCreate, UserMiner
+)
